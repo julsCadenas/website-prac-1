92ffa844279e0b68ad77da620f51d4a05a0c5fe9
Julian Cadenas <jscadenas12@gmail.com>
2023-09-09 17:01:42 +0800

    javascript file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+window.addEventListener("load",showPic);
+document.getElementById("gayButton").addEventListener("click",yoMom);   
+gayButton();
+
+function yoMom(){
+    alert("YOMOM GAY LMAOOOOO");
+}
+
+function showPic(){
+    let gayPic=document.getElementById("image");
+    gayPic.src="the rock.jpg";
+}
+
+function gayButton() {
+    let yesAns=document.getElementById("yesButton").addEventListener("click", function(){
+        alert("u r gay HAHAHAHAHAHAHA");
+    })
+    let noAns=document.getElementById("noButton").addEventListener("click", function(){
+        alert("lol what a shame");
+    })
+}
