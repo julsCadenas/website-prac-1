aad02c7f09d9e714293f53a87ba605e86c0468b3
Julian Cadenas <jscadenas12@gmail.com>
2023-12-09 16:30:07 +0800

    -

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ window.addEventListener("load",showPic);
 document.getElementById("gayButton").addEventListener("click",yoMom);   
 gayButton();
 
+link="https://youtu.be/4n4rBrs5-LY?si=t7ZLx-XrdiLP_69z&t=11";
+
 function yoMom(){
-    alert("YOMOM GAY LMAOOOOO");
+    //alert("YOMOM GAY LMAOOOOO");
+    window.open(link,'_blank');
 }
 
 function showPic(){
     let gayPic=document.getElementById("image");
-    gayPic.src="the rock.jpg";
+    gayPic.src="/images/the rock.jpg";
 }
 
 function gayButton() {
